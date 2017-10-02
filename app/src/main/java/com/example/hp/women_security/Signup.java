c375ff60590354335c3d3ad196589eafe8a47132
surajsaluja <suraj.saluja.4@gmail.com>
2017-10-02 18:09:12 +0530

    okok

--- app/src/main/java/com/example/hp/women_security/Signup.java
+++ app/src/main/java/com/example/hp/women_security/Signup.java
@@ -3,9 +3,15 @@ package com.example.hp.women_security;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextUtils;
+import android.text.TextWatcher;
+import android.util.Patterns;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class Signup extends AppCompatActivity {
 Button sign;
@@ -20,12 +26,18 @@ Button sign;
         phone=(EditText)findViewById(R.id.Phone);
         pass=(EditText)findViewById(R.id.password);
         confirm=(EditText)findViewById(R.id.confirm);
+
+
+        name.addTextChangedListener(new MyTextWatcher(name));
+        email.addTextChangedListener(new MyTextWatcher(email));
+        pass.addTextChangedListener(new MyTextWatcher(pass));
+        phone.addTextChangedListener(new MyTextWatcher(phone));
+
         sign.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent i=new Intent(Signup.this,Resetpass.class);
-                startActivity(i);
 
+                submitForm();
 
             }
         });
@@ -42,43 +54,61 @@ Button sign;
         if (!validatePassword()) {
             return;
         }
+        if (!validatephone()) {
+            return;
+        }
 
         Toast.makeText(getApplicationContext(), "Thank You!", Toast.LENGTH_SHORT).show();
     }
 
     private boolean validateName() {
-        if (inputName.getText().toString().trim().isEmpty()) {
-            inputLayoutName.setError(getString(R.string.err_msg_name));
-            requestFocus(inputName);
+        if (name.getText().toString().trim().isEmpty()) {
+            name.setError(getString(R.string.err_msg_name));
+            requestFocus(name);
             return false;
         } else {
-            inputLayoutName.setErrorEnabled(false);
+            //name.setErrorEnabled(false);
         }
 
         return true;
     }
 
     private boolean validateEmail() {
-        String email = inputEmail.getText().toString().trim();
+        String emails = email.getText().toString().trim();
 
-        if (email.isEmpty() || !isValidEmail(email)) {
-            inputLayoutEmail.setError(getString(R.string.err_msg_email));
-            requestFocus(inputEmail);
+        if (emails.isEmpty() || !isValidEmail(emails)) {
+            email.setError(getString(R.string.err_msg_email));
+            requestFocus(email);
             return false;
         } else {
-            inputLayoutEmail.setErrorEnabled(false);
+            //inputLayoutEmail.setErrorEnabled(false);
+        }
+
+        return true;
+    }
+
+    private boolean validatephone() {
+
+String phno=phone.getText().toString();
+
+        if (phno.isEmpty() || !isValidphoneno(phno)) {
+            phone.setError(getString(R.string.err_msg_phone));
+            requestFocus(phone);
+            return false;
+        } else {
+            //inputLayoutEmail.setErrorEnabled(false);
         }
 
         return true;
     }
 
     private boolean validatePassword() {
-        if (inputPassword.getText().toString().trim().isEmpty()) {
-            inputLayoutPassword.setError(getString(R.string.err_msg_password));
-            requestFocus(inputPassword);
+        if (pass.getText().toString().trim().isEmpty()) {
+            pass.setError(getString(R.string.err_msg_password));
+            requestFocus(pass);
             return false;
         } else {
-            inputLayoutPassword.setErrorEnabled(false);
+            //pass.setErrorEnabled(false);
         }
 
         return true;
@@ -88,6 +118,9 @@ Button sign;
         return !TextUtils.isEmpty(email) && android.util.Patterns.EMAIL_ADDRESS.matcher(email).matches();
     }
 
+    private static boolean isValidphoneno(String phno) {
+        return !TextUtils.isEmpty(phno) && Patterns.PHONE.matcher(phno).matches();
+    }
     private void requestFocus(View view) {
         if (view.requestFocus()) {
             getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
@@ -110,15 +143,17 @@ Button sign;
 
         public void afterTextChanged(Editable editable) {
             switch (view.getId()) {
-                case R.id.input_name:
+                case R.id.Name:
                     validateName();
                     break;
-                case R.id.input_email:
+                case R.id.email:
                     validateEmail();
                     break;
-                case R.id.input_password:
+                case R.id.password:
                     validatePassword();
                     break;
+                case R.id.Phone:
+                    validatephone();
             }
         }
     }
