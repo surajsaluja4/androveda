a2944ffd7faccdcb101588d3c6ac26682a4f273d
surajsaluja <suraj.saluja.4@gmail.com>
2017-10-02 14:29:28 +0530

    update

--- app/src/main/java/com/example/hp/women_security/Resetpass.java
+++ app/src/main/java/com/example/hp/women_security/Resetpass.java
@@ -1,13 +1,26 @@
 package com.example.hp.women_security;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Resetpass extends AppCompatActivity {
-
+Button reset;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_resetpass);
+    reset=(Button)findViewById(R.id.reset_final);
+        reset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent i = new Intent(Resetpass.this, Loggedin.class);
+                startActivity(i);
+
+            }
+        });
     }
 }
--- app/src/main/java/com/example/hp/women_security/Signup.java
+++ app/src/main/java/com/example/hp/women_security/Signup.java
@@ -1,13 +1,24 @@
 package com.example.hp.women_security;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Signup extends AppCompatActivity {
-
+Button sign;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
+        sign=(Button)findViewById(R.id.signup);
+        sign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i=new Intent(Signup.this,Resetpass.class);
+                startActivity(i);
+            }
+        });
     }
 }
